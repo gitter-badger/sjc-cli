21d930031d473107b23e0971040c52d74649ca38
Sean Macdonald <sean@crazyhorsecoding.com>
2015-09-18 16:21:57 -0400

    improved installation instructions

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # SJC cli
 
 A command-line utility for SJC
+
+## Installation
+
+    curl https://raw.githubusercontent.com/stjosephcontent/sjc-cli/master/install.sh | bash
--- install.sh
+++ install.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-mkdir -p ~/.sjc/cli
-rm -rvf ~/.sjc/cli
+sudo -v
+
+sudo -H mkdir -p ~/.sjc/cli
+sudo -H rm -rvf ~/.sjc/cli
 
 cd ~/.sjc
 
 git clone https://github.com/stjosephcontent/sjc-cli.git cli
 
-npm install n
-n latest
+cd cli
 
-npm install
-npm link
+sudo -H npm install n -g
+sudo -H n latest
+
+sudo -H npm install
+sudo -H npm link
 
 echo "ok, i think orchestra is up and running. try 'sjc up'"
-
 
