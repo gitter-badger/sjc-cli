5a267fd96ee702c2d1df1fc5e06aa581b960f05f
Anirudh Swaminathan <aswaminathan@P15013-10-2-101-254.vlan-2.stjosephcontent.com>
2015-09-21 10:34:37 -0400

    updates

--- cli.sh
+++ cli.sh
@@ -2,6 +2,6 @@
 
 export SJC_ROOT=$HOME/.sjc;
 export SJC_CLI_ROOT=$SJC_ROOT/cli;
-export SJC_ORCHESTRA_VESRION=0.0.1
+export SJC_ORCHESTRA_VERSION=0.0.1
 
-node --harmony $SJC_CLI_ROOT/src/cli.js $@;
+node $SJC_CLI_ROOT/src/cli.js $@;
