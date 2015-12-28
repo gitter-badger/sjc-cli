e556055b5c02d2eb3b1be77dd3c483c5ccb57b7d
Sean Macdonald <sean.macdonald@stjoseph.com>
2015-12-28 07:32:15 +0000

    using environment vars to distinguish hosts

--- cli.sh
+++ cli.sh
@@ -2,6 +2,18 @@
 
 export SJC_ROOT=$HOME/.sjc;
 export SJC_CLI_ROOT=$SJC_ROOT/cli;
-export SJC_ORCHESTRA_VERSION=0.0.1
 
-node $SJC_CLI_ROOT/src/cli.js $@;
+env_vars=""
+
+#   pick up SJC environment variables
+if [ -f ~/.sjc/env ]; then
+    env_vars="$env_vars $(cat ~/.sjc/env)"
+fi
+if [ -f ~/.sjc/reverseproxy/env ]; then
+    env_vars="$env_vars $(cat ~/.sjc/reverseproxy/env)"
+fi
+#   convert newlines to spaces, and trim
+env_vars="$(echo $env_vars | tr '\n' ' ' | sed -e 's/\s*$//' | sed -e 's/^\s*//' | sed -e 's/\s{2,}/ /')"
+
+eval "$env_vars node $SJC_CLI_ROOT/src/cli.js $@"
+
