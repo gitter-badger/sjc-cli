260908b223414fdda1a5e1367bd28aa4c33b5750
Sean Macdonald <sean.macdonald@stjoseph.com>
2016-01-01 13:31:49 -0500

    improvements to picking up env vars

--- cli.sh
+++ cli.sh
@@ -3,17 +3,14 @@
 export SJC_ROOT=$HOME/.sjc;
 export SJC_CLI_ROOT=$SJC_ROOT/cli;
 
-env_vars=""
-
 #   pick up SJC environment variables
-if [ -f ~/.sjc/env ]; then
-    env_vars="$env_vars $(cat ~/.sjc/env)"
+env_vars=""
+if [ -e ~/.sjc/env ]; then
+    env_vars+="$(cat ~/.sjc/env)" 
 fi
-if [ -f ~/.sjc/reverseproxy/env ]; then
-    env_vars="$env_vars $(cat ~/.sjc/reverseproxy/env)"
+if [ -e ~/.sjc/reverseproxy/env ]; then
+    env_vars+="$(cat ~/.sjc/reverseproxy/env)"
 fi
-#   convert newlines to spaces, and trim
-env_vars="$(echo $env_vars | tr '\n' ' ' | sed -e 's/\s*$//' | sed -e 's/^\s*//' | sed -e 's/\s{2,}/ /')"
+env_vars="$(echo "$env_vars" | sed -e 's/#.*//' | tr '\n' ' ' | sed -e 's/\s*$//' | sed -e 's/^\s*//' | sed -e 's/\s{2,}/ /')"
 
 eval "$env_vars node $SJC_CLI_ROOT/src/cli.js $@"
-
